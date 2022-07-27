0b65b11b511338d57d2c7bff5e50fc56ddc171e4
rosharp <ro.moreko@gmail.com>
2022-07-27 12:14:46 +0300

    Mobile nav and layout

--- src/App.js
+++ src/App.js
@@ -1,45 +1,107 @@
-import logo from './logo.svg';
+import React from "react";
 import './css/style.css';
-import Navbar from './components/Navbar';
 
 function App() {
-  return (
-    <div className="App">
-      <section className="main">
-        <Navbar />
+	const [isActive, setActive] = React.useState(false);
+	const [isToggledFeatures, setToggledFeatures] = React.useState(false);
+	const [isToggledCompany, setToggledCompany] = React.useState(false);
 
-        <div className="body">
+	const toggleFeatures = () => {
+		setToggledFeatures(!isToggledFeatures);
+	}
 
-          <img className="main-hero-img" src="images/image-hero-desktop.png" />
+	const toggleCompany = () => {
+		setToggledCompany(!isToggledCompany);
+	}
 
-          <div className="text-block">
-            <div className="body-text">
-              <h1>Make remote work</h1>
-              <p>
-                Get your team in sync, no matter your location. Streamline processes,
-                create team rituals, and watch productivity soar.
-              </p>
-              <button className="black-btn">Learn more</button>
-            <div className="sponsors-img">
-              <img src="/images/client-databiz.svg" />
-              <img src="/images/client-audiophile.svg" />
-              <img src="/images/client-meet.svg" />
-              <img src="/images/client-maker.svg" />
-            </div>
-          </div>
+	const toggleActive = () => {
+		setActive(!isActive);
+	}
 
-          </div>
-        </div>
+	return (
+		<div className="App">
+			<section className={isActive ? "main overlay" : "main"}>
 
-      </section>
+				<button
+					id="mobile-nav-btn"
+					className={isActive ? "mobile-nav-toggle-inactive" : "mobile-nav-toggle"}
+					aria-controls="navbar"
+					aria-expanded="false"
+					onClick={toggleActive}
+				>
+					<span className="sr-only">Menu</span>
+				</button>
 
-      <div className="attribution">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-        Coded by <a href="#">Your Name Here</a>.
-      </div>
-     
-    </div>
-  );
+				<nav className="navbar" data-visible={isActive ? "false" : "true"}>
+					<img className="logo-img" src="/images/logo.svg" />
+					<ul className="main-nav">
+						<li>
+							<a className="nav-anchor" aria-controls="features-list" onClick={toggleFeatures}>
+								Features
+								<span><img className="nav-arrow-icon" src={isToggledFeatures ? "/images/icon-arrow-up.svg" : "/images/icon-arrow-down.svg"} /></span>
+							</a>
+							<ul className="features-list" data-visible={isToggledFeatures ? "true" : "false"}>
+								<li><a href='#'><span><img src="/images/icon-todo.svg" /></span>Todo List</a></li>
+								<li><a href='#'><span><img src="/images/icon-calendar.svg" /></span>Calendar</a></li>
+								<li><a href='#'><span><img src="/images/icon-reminders.svg" /></span>Reminders</a></li>
+								<li><a href='#'><span><img src="/images/icon-planning.svg" /></span>Planning</a></li>
+							</ul>
+						</li>
+						<li>
+							<a className="nav-anchor" onClick={toggleCompany}>
+								Company
+								<span><img className="nav-arrow-icon" src={isToggledCompany ? "/images/icon-arrow-up.svg" : "/images/icon-arrow-down.svg"} /></span>
+							</a>
+							<ul className="company-list" data-visible={isToggledCompany ? "true" : "false"}>
+								<li><a href='#'>History</a></li>
+								<li><a href='#'>Team</a></li>
+								<li><a href='#'>Blog</a></li>
+							</ul>
+						</li>
+						<li><a href="">Career</a></li>
+						<li><a href="">About</a></li>
+					</ul>
+					<ul className="login-nav">
+						<li><a href="">Login</a></li>
+						<li><a className="black-outline-btn" href="">Register</a></li>
+					</ul>
+				</nav>
+
+				<div className="body" >
+
+					<picture>
+						<source srcSet="images/image-hero-mobile.png" media="(max-width: 1000px)" />
+						<img className="main-hero-img" src="images/image-hero-desktop.png" />
+					</picture>
+
+					<div className="text-block">
+						<div className="body-text">
+							<h1>Make remote work</h1>
+							<p>
+								Get your team in sync, no matter your location. Streamline processes,
+								create team rituals, and watch productivity soar.
+							</p>
+							<button className="black-btn">Learn more</button>
+							<div className="sponsors-img">
+								<img src="/images/client-databiz.svg" />
+								<img src="/images/client-audiophile.svg" />
+								<img src="/images/client-meet.svg" />
+								<img src="/images/client-maker.svg" />
+							</div>
+						</div>
+
+					</div>
+				</div>
+
+			</section>
+
+			<div className="attribution">
+				Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+				Coded by <a href="#">Your Name Here</a>.
+			</div>
+
+		</div>
+	);
 }
 
 export default App;
